603b706442ea0f48c477547525afab61398a0f6e
koie <koie3211@gmail.com>
2021-11-11 23:32:56 +0800

    music search paginate

--- app/Http/Controllers/Music/V1/MusicController.php
+++ app/Http/Controllers/Music/V1/MusicController.php
@@ -5,18 +5,25 @@ namespace App\Http\Controllers\Music\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Music\MusicResource;
 use App\Models\Music\Music;
+use Illuminate\Http\Request;
 
 class MusicController extends Controller
 {
-	public function index()
+	public function index(Request $request)
 	{
-		//  TODO: 分頁、搜尋...
-		return response()->json([
-			'status' => true,
-			'data' => MusicResource::collection(
-				Music::with('musicType:id,name')->where('status', true)->orderBy('sort')->get()
-			)
-		]);
+		// 取得搜尋字串
+		$search = $request->input('search', '');
+
+		// 每頁筆數
+		$limit = (int) $request->input('limit', 18);
+
+		return MusicResource::collection(
+			Music::with(['musicType' => function ($query) {
+				$query->select(['id', 'name'])->where('status', true);
+			}])->when($search, function ($query) use ($search) {
+				$query->where('name', 'like', "%{$search}%")->orWhere('composer', 'like', "%{$search}%");
+			})->where('status', true)->orderBy('sort')->paginate($limit)->withQueryString()
+		);
 	}
 
 	public function show(Music $music)
